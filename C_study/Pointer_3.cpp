124702c00a0da88e109c23852dda3e0aac448afc
Li-Haoyuan <50567458+Li-Haoyuan@users.noreply.github.com>
2019-08-27 09:46:46 +0900

    Add files via upload

---
+++ C_study/Pointer_3.cpp
@@ -0,0 +1,50 @@
+﻿///* 经典指针程序 两个数互换 */
+//#include <stdio.h>
+//
+//void change_1(int, int);
+//void change_2(int*, int*);
+//void change_3(int*, int*);
+//
+//void main(void) {
+//
+//	int a = 5;
+//	int b = 10;
+//
+//	//change_1(a,b);
+//	//change_2(&a, &b);
+//	change_3(&a, &b);
+//
+//	printf("a = %d, b = %d", a, b);
+//}
+//
+//
+////这个方法不能完成互换，因为是对形参进行的互换操作
+//void change_1(int p,int q) {
+//
+//	int t;
+//
+//	t = p;
+//	p = q;
+//	q = t;
+//
+//}
+//
+////这个方法也不能完成互换,只是互换了变量里的 a b 的地址
+//void change_2(int * p, int * q) {
+//
+//	int * t;
+//
+//	t = p;
+//	p = q;
+//	q = t;
+//}
+//
+//void change_3(int* p, int* q) {
+//
+//	int t;
+//
+//	t = *p;
+//	*p = *q;
+//	*q = t;
+//
+//}
---
+++ C_study/if_2.cpp
@@ -0,0 +1,26 @@
+﻿//# include <stdio.h>
+//
+//int main(void) {
+//	//简单的冒泡排序
+//	int a, b, c;
+//	int t;
+//	printf("输入三个数字:");
+//	scanf_s("%d %d %d", &a, &b, &c);
+//	if (a < b) {
+//		t = a;
+//		a = b;
+//		b = t;
+//	}
+//	if (a < c) {
+//		t = c;
+//		c = a;
+//		a = t;
+//	}
+//	if (b < c) {
+//		t = b;
+//		b = c;
+//		c = t;
+//	}
+//
+//	printf("%d %d %d\n",a,b,c);
+//}
