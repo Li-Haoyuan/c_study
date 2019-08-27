835e494b7a997ccb874d2561824124fc4630566f
li-haoyuan <lichade429jp@gmail.com>
2019-08-27 16:28:07 +0900

    no message

---
+++ C_study/Pointer_6.cpp
@@ -0,0 +1,16 @@
+﻿/*
+#include <stdio.h>
+#include <malloc.h>
+
+void main(void) {
+	//多级指针
+	int i = 10;
+	int* p = &i; //存int的地址的变量
+	int** q = &p; //存int*地址的变量
+	int*** r = &q; //存int**地址的变量
+
+	//r = &p; 类型错误
+	//***r = i;
+
+}
+*/
---
+++ C_study/Pointer_7.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+
+void f(int ** q) { //q也是指针变量 所以占4(8)字节
+	int i = 5;
+	//*q等价于p
+	//*q = i; error 类型错误
+	*q = &i;
+}
+
+void main(void) {
+	int* p;
+	f(&p);
+
+	printf("%d", *p);
+}
